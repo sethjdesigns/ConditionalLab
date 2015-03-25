afe139617f0d50f175e18196570b460d17bb4e1a
Seth Johnson <sethjdesigns@gmail.com>
2015-03-25 13:12:08 -0400

    Init FizzBuzz
    Fizzing and Buzzing and whatnot

--- ConditionalLab/Program.cs
+++ ConditionalLab/Program.cs
@@ -67,6 +67,7 @@ namespace ConditionalLab
              TIME CLASSIFICATIONS
             */
 
+            /*
             DateTime saveNow = DateTime.Now;
             int seconds = saveNow.Second;
             Console.WriteLine("This time is {0}", saveNow);
@@ -92,10 +93,34 @@ namespace ConditionalLab
             {
                 Console.WriteLine("Working on it!");
             }
+             
+            */
 
             /*
              * Fizz Buzz
              */
+
+           
+            for (int i = 0; i < 100; i++) 
+            {
+                if (i % 3 == 0) 
+                {
+                    Console.WriteLine("Fizz");
+                }
+
+                else if (i % 5 == 0) 
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 5 == 0 && i % 15 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else 
+                {
+                    Console.WriteLine(i);
+                }
+            }
 
             Console.ReadLine();
         }
